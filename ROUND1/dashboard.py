d2bd082ffa7dd481251b794530f76ff36ae46f88
Ben <ben.y.chang@outlook.com>
2026-04-14 17:07:37 +0100

    first commit

---
+++ ROUND1/dashboard.py
@@ -0,0 +1,108 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+st.set_page_config(page_title="IMC Prosperity - Round 1 Dashboard", layout="wide")
+st.title("IMC Prosperity - Round 1 Dashboard")
+
+@st.cache_data
+def load_data():
+    prices_dfs = []
+    prices_dfs.append(pd.read_csv("prices_round_1_day_0.csv", sep=";"))
+    prices_dfs.append(pd.read_csv("prices_round_1_day_-1.csv", sep=";"))
+    prices_dfs.append(pd.read_csv("prices_round_1_day_-2.csv", sep=";"))
+    # Load Trades
+    trades_dfs = []
+    trades_dfs.append(pd.read_csv("trades_round_1_day_0.csv", sep=";"))
+    trades_dfs.append(pd.read_csv("trades_round_1_day_-1.csv", sep=";"))
+    trades_dfs.append(pd.read_csv("trades_round_1_day_-2.csv", sep=";"))
+
+    return prices_dfs, trades_dfs
+
+product = st.selectbox("Select product:", ["Ash-coated osmium", "Intarian pepper root"])
+day = st.selectbox("Select day:", [0, -1, -2])
+prices_dfs, trades_dfs = load_data()
+
+# Sidebar for options
+with st.sidebar:
+    st.header("Settings")
+    circle_size = st.slider("Circle size", min_value=0, max_value=50, step=1, value=15)
+    circle_opacity = st.slider("Circle opacity", min_value=0, max_value=10, step=1, value=7)
+
+if product == "Ash-coated osmium":
+    st.header("Market data for ash-coated osmium")
+    prices_df = prices_dfs[-day]
+    prices_df = prices_df[(prices_df["product"] == "ASH_COATED_OSMIUM") & (prices_df["bid_price_1"] != 0)]
+    trades_df = trades_dfs[-day]
+    trades_df = trades_df[trades_df["symbol"] == "ASH_COATED_OSMIUM"]
+
+    # Plot market data
+    fig = go.Figure()
+
+    # Bid line
+    fig.add_trace(go.Scatter(
+        x=prices_df["timestamp"], 
+        y=prices_df["bid_price_1"], 
+        name="Bid Price",
+        mode="lines",
+        line=dict(color="blue")
+    ))
+
+    # Ask line
+    fig.add_trace(go.Scatter(
+        x=prices_df["timestamp"], 
+        y=prices_df["ask_price_1"], 
+        name="Ask Price",
+        mode="lines",
+        line=dict(color="red")
+    ))
+
+    # Trade data
+    fig.add_trace(go.Scatter(
+        x=trades_df["timestamp"], 
+        y=trades_df["price"],
+        mode="markers",
+        marker=dict(color="black", size=circle_size*trades_df["quantity"]/10, opacity=circle_opacity/10)
+    ))
+
+    st.plotly_chart(fig)
+
+elif product == "Intarian pepper root":
+    st.header("Market data for Intarian pepper root")
+    prices_df = prices_dfs[-day]
+    prices_df = prices_df[(prices_df["product"] == "INTARIAN_PEPPER_ROOT") & (prices_df["bid_price_1"] != 0)]
+    trades_df = trades_dfs[-day]
+    trades_df = trades_df[trades_df["symbol"] == "INTARIAN_PEPPER_ROOT"]
+
+    # Plot market data
+    fig = go.Figure() 
+
+    # Bid line
+    fig.add_trace(go.Scatter(
+        x=prices_df["timestamp"], 
+        y=prices_df["bid_price_1"], 
+        name="Bid Price",
+        mode="lines",
+        line=dict(color="blue")
+    ))
+
+    # Ask line
+    fig.add_trace(go.Scatter(
+        x=prices_df["timestamp"], 
+        y=prices_df["ask_price_1"], 
+        name="Ask Price",
+        mode="lines",
+        line=dict(color="red")
+    ))
+
+    # Trade data
+    fig.add_trace(go.Scatter(
+        x=trades_df["timestamp"], 
+        y=trades_df["price"],
+        mode="markers",
+        marker=dict(color="black", size=circle_size*trades_df["quantity"]/10, opacity=circle_opacity/10)
+    ))
+
+    st.plotly_chart(fig)
